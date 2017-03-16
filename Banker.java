904732e2fc9bb988d5b549ac499cfc94435b5036
nitya21 <nityajitani@gmail.com>
2017-03-16 22:32:39 +0530

    Add files via upload

---
+++ Banker.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class Banker{
+int max[][]=new int[10][10];
+int alloc[][]=new int[10][10];
+int avail[][]=new int[10][10];
+int need[][]=new int[10][10];
+
+int p,r,flag;
+public static void main(String args[]){
+
+Scanner sc=new Scanner(System.in);
+System.out.println("enter number of processes/n");
+p=sc.nextInt();
+System.out.println("enter no of resources/n");
+r=sc.nextInt();
+System.out.println("enter the max matrix/n");
+for(int i=0;i<p;i++){
+	for(int j=0;j<r;j++)
+	{
+		max[i][j]=Integer.parseInt(obj.readLine());
+	}
+}
+System.out.println("enter the alloc matrix/n");
+for(int i=0;i<p;i++){
+	for(int j=0;j<r;j++)
+	{
+		alloc[i][j]=Integer.parseInt(obj.readLine());
+	}
+}
+System.out.println("enter the alloc matrix/n");
+	for(int j=0;j<r;j++)
+	{
+		avail[0][j]=Integer.parseInt(obj.readLine());
+	}
+for(int i=0;i<p;i++){
+	for(int j=0;j<r;j++)
+	{
+		need[i][j]=max[i][j]-alloc[i][j];
+	}
+}
+for(int i=0;i<p;i++){
+	for(int j=0;j<r;j++)
+	{
+		if(need[i][j]<=avail[0][j])
+		{
+			continue;
+	}
+        flag=0;
+	}
+	for(int j=0;j<r;j++)
+	{
+		if(flag==0 && need[i][j]!=100){
+			avail[0][j]=avail[0][j]+alloc[i][j];
+			need[i][j]=100;
+		}
+		else {
+			continue;
+	}
+}
+}
+for(int i=0;i<p;i++){
+	for(int j=0;j<r;j++)
+	{
+		if(need[i][j]==100){
+			System.out.println("safe state");
+		}
+	}
+}
+}
+}
